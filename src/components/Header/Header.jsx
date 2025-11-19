49e72f1caf640b194c67da644b40eb44e2050a2d
renaud.jacqmotte <rjacqmotte@gmail.com>
2025-11-19 10:47:20 +0100

    create components files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1 className="header__title">Jammming</h1>
+            <h2 className="header__subtitle">Playlists Maker</h2>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Playlist/PlayList.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Playlist() {
+    return (
+        <div>
+            <h1>je suis une playlist</h1>
+        </div>
+    );
+}
+
+export default PlayList;
