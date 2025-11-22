db19d43b7dec73b9e3e96e254c45a0a143d2b507
rjacqmotte <rjacqmotte@gmail.com>
2025-11-22 11:58:25 +0100

    define Trackist.jsx and .css

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,7 +1,25 @@
 import styles from './TrackList.module.css'
+import Track from '../Track/Track'
 
 function TrackList() {
-    return ()
+    return (
+        <div className={styles.trackListContainer}>
+            <h2 calssName={styles.trackListTitle}>Select</h2>
+            <ul className={styles.trackList}>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+                <li className={styles.trackListItem}><Track /></li>
+            </ul>
+        </div>
+    )
 }
 
 export default TrackList
