2c3baddac2b79ee043a71e27c822d183fc85464a
rjacqmotte <rjacqmotte@gmail.com>
2025-11-23 15:31:59 +0100

    create saveForm component

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -2,13 +2,14 @@ import styles from './MainView.module.css'
 import Menu from '../Menu/Menu'
 import SearchBar from '../SearchBar/SearchBar'
 import TrackList from '../TrackList/TrackList'
+import SaveForm from '../SaveForm/SaveForm'
 
 function MainView() {
     return (
         <div className={styles.mainView}>
             <Menu />
             <div className={styles.mainViewContainer}>
-                <TrackList/>
+                <SaveForm/>
             </div>
         </div>
     )
---
+++ src/components/SaveForm/SaveForm.jsx
@@ -0,0 +1,27 @@
+import styles from './SaveForm.module.css'
+
+function SaveForm() {
+    return (
+        <div className={styles.saveFormContainer}>
+            <h2 calssName={styles.saveFormTitle}>Export</h2>
+            <form className={styles.saveForm}>
+                <div className={styles.saveFormBlock}>
+                    <label htmlFor='name' className={styles.saveFormLabel}>Name</label>
+                    <input id='name' type='text' className={styles.saveFormInput} />
+                </div>
+                <div className={styles.saveFormBlock}>
+                    <label htmlFor='email' className={styles.saveFormLabel}>Email</label>
+                    <input id='email' type='email' className={styles.saveFormInput} />
+                </div>
+                <div className={styles.saveFormBlock}>
+                    <label htmlFor='password' className={styles.saveFormLabel}>Password</label>
+                    <input id='password' type='password' className={styles.saveFormInput} />
+                </div>
+                <button className={styles.saveFormBtn} type='submit'>Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default SaveForm
