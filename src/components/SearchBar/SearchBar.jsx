624debc5933b6145edd2c568938fa6588bf68cef
rjacqmotte <rjacqmotte@gmail.com>
2025-12-03 16:02:16 +0100

    handleUserInput and handleSubmit in SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,23 @@
 import styles from './SearchBar.module.css'
+import { useState } from 'react'
 
 function SearchBar() {
+    const [userInput, setUserInput] = useState()
+
+    function handleUserInput(e) {
+        setUserInput(e.target.value)
+    }
+
+    function handleSubmit() {
+        console.log('handlesubmit acitf')
+        e.preventDefault()
+        alert('test')
+    }
+
     return (
-            <form className={styles.searchBar}>
-                <input placeholder='cherche une chanson pour ta playlist'></input>
-                <button>Search</button>
+            <form className={styles.searchBar} onSubmit={handleSubmit}>
+                <input id="searchBar" type="text" onChange={handleUserInput} value={userInput} placeholder='cherche une chanson pour ta playlist'></input>
+                <button type="submit">Search</button>
             </form>
 
     )
