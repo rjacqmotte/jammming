ee4d567aa12f83f83c9c08a559a543dc3e0eb2b6
renaud <rjacqmotte@gmail.com>
2025-12-06 18:03:14 +0100

    start apiLogic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import styles from './Header.module.css';
 
 function Header() {
+  const test = import.meta.env.VITE_TEST || 'Test par défaut';
+
+  console.log('Toutes les variables:', import.meta.env);
+  console.log('Test:', import.meta.env.REACT_APP_TEST);
+
   return (
     <header className={styles.header}>
-      <h1 className={styles.headerTitle}>Jammming</h1>
+      <h1 className={styles.headerTitle}>{test}</h1>
       <h2 className={styles.headerSubtitle}>Playlists Maker</h2>
     </header>
   );
