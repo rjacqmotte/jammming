98a8039a48841e440486786679f869aea8c4ee3f
renaud.jacqmotte <rjacqmotte@gmail.com>
2025-12-23 08:30:23 +0100

    configure button s states with object mapping

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,38 +1,13 @@
 import styles from './Button.module.css';
 
+const BUTTON_CONFIG = {
+  normal: { content: '+', className: styles.trackAddButton },
+  isSelected: { content: 'V', className: `${styles.trackButtonAdd} ${styles.trackButtonSelected}` }
+};
+
 function Button({ state = 'isSelected' }) {
-  switch (state) {
-      case 'normal': 
-      return (
-          <button className={styles.trackAddButton}>
-          <span>+</span>
-          </button>
-        );
-      case 'isSelected':
-         return (          
-          <button className={`${styles.trackAddButton} ${styles.trackAddButtonSelected}`}>
-          <span>V</span>
-          </button>
-        );
-          default:
-      return (
-        <button className={styles.trackAddButton}>
-          <span>+</span>
-        </button>
-      );
-    }
+  const config = BUTTON_CONFIG[state];
+  return <button className={config.className}><span>{config.content}</span></button>;
 }
 
 export default Button;
-
-
-/** a essayer après! 
-  const BUTTON_CONFIG = {
-  normal: { content: '+', className: styles.trackAddButton },
-  isSelected: { content: 'V', className: `${styles.trackAddButton} ${styles.trackButtonSelected}` }
-};
-
-function Button({ state = 'normal' }) {
-  const config = BUTTON_CONFIG[state];
-  return <button className={config.className}><span>{config.content}</span></button>;
-} */
