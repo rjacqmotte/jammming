5a148b946fbf826800c21ad36b4533374bedb087
renaud.jacqmotte <rjacqmotte@gmail.com>
2025-12-23 08:23:02 +0100

    configure Button s states with switch case

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,11 +1,38 @@
 import styles from './Button.module.css';
 
-function Button() {
-  return (
-    <button className={styles.trackAddButton}>
-      <span>+</span>
-    </button>
-  );
+function Button({ state = 'isSelected' }) {
+  switch (state) {
+      case 'normal': 
+      return (
+          <button className={styles.trackAddButton}>
+          <span>+</span>
+          </button>
+        );
+      case 'isSelected':
+         return (          
+          <button className={`${styles.trackAddButton} ${styles.trackAddButtonSelected}`}>
+          <span>V</span>
+          </button>
+        );
+          default:
+      return (
+        <button className={styles.trackAddButton}>
+          <span>+</span>
+        </button>
+      );
+    }
 }
 
 export default Button;
+
+
+/** a essayer après! 
+  const BUTTON_CONFIG = {
+  normal: { content: '+', className: styles.trackAddButton },
+  isSelected: { content: 'V', className: `${styles.trackAddButton} ${styles.trackButtonSelected}` }
+};
+
+function Button({ state = 'normal' }) {
+  const config = BUTTON_CONFIG[state];
+  return <button className={config.className}><span>{config.content}</span></button>;
+} */
