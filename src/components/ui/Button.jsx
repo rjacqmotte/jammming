ad542aa713d062eed27458f29a6ac538448ef5d3
renaud.jacqmotte <rjacqmotte@gmail.com>
2025-12-23 07:38:51 +0100

    center + inn the button in Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,11 @@
 import styles from './Button.module.css';
 
 function Button() {
-  return <button className={styles.trackAddButton}>+</button>;
+  return (
+    <button className={styles.trackAddButton}>
+      <span>+</span>
+    </button>
+  );
 }
 
 export default Button;
