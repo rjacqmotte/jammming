846ffcedffd1a5d3e27ff882438c6c0dd2feb699
rjacqmotte <rjacqmotte@gmail.com>
2026-01-08 11:03:46 +0100

    add log out button to header, no logic connected

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import styles from './Header.module.css';
+import Button2 from '../ui/Button2';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.headerTitle}>Jammming</h1>
-      <h2 className={styles.headerSubtitle}>Playlists Maker</h2>
+      <div>
+        <h1 className={styles.headerTitle}>Jammming</h1>
+        <p className={styles.headerSubtitle}>Playlists Maker</p>
+      </div>
+      <div>
+      <Button2 style="text" content="Log out" />
+      <p className={styles.headerSubtitle}>LastFM</p>
+      </div>
     </header>
   );
 }
