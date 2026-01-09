e5db2e8f8ad863c97e8bd3c6e1263f68269e2913
rjacqmotte <rjacqmotte@gmail.com>
2026-01-09 11:07:58 +0100

    upgrade Footer.jsx with conditionnal config

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,23 @@
 import styles from './Footer.module.css';
 import Button2 from '../ui/Button2';
 
-const FOOTER_CONFIG = {
-    
+const FOOTER_CONFIG = { 
+    connect: {},
+    searchBar: { 
+        classVariant: styles.justifyEnd,
+        content: ['Show last results'],
+    },
+    trackList: {},
+    playlist: {},
+    saveForm: {},
 };
+
+const config = FOOTER_CONFIG['searchBar'];
 
 function Footer() {
     return (
-        <footer className={styles.Footer}>
-          <Button2 style='text' content='Show last results' />
+        <footer className={`${styles.footer} ${config.classVariant}`}>
+          <Button2 style='text' content={config.content[0]} />
         </footer>
     );
 }
